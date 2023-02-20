a66b8c27a7b935d3051913dec88981508593a1ee
AmiraKar <amirakarmous@hotmail.co.uk>
2023-02-20 09:11:08 +0000

    Add project files.

---
+++ Lesson8/MyTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Lesson8
+{
+   
+        class MyTest
+        {
+
+            [Test]
+            public void AreaCTest()
+            {
+                Circle circle = new Circle();
+
+                
+                double expectedValue = 12.56;
+                double actualValue = circle.AreaC();
+                Assert.AreEqual(expectedValue,  actualValue);
+
+            }
+            [Test]
+            public void AreaTTest()
+            {
+                Triangle triangle = new Triangle();
+                double a = 1;
+                double b = 2;
+                double c = 3;
+
+                double expectedValue = 4;
+                double actualValue = triangle.AreaT(a, b, c);
+                Assert.AreEqual(expectedValue, actualValue);
+
+            }
+        }
+
+
+    }
+}
---
+++ Lesson8/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson8
+{
+    public class Circle
+    {
+        public double radiusR;
+        public double areaCircle;
+        public double perimeterCircle;
+        public void AreaC(double radiusR)
+        {
+            areaCircle = 3.14 * radiusR;
+            Console.WriteLine(areaCircle);
+            
+        }
+        public void PerimeterC(double radiusR)
+        {
+            perimeterCircle = 2 * 3.14 * radiusR;
+            Console.WriteLine(perimeterCircle);
+         
+        }
+    }
+    public class Rectangle
+    {
+        public double heightH;
+        public double widthW;
+        public double areaR;
+        public double perimeterR;
+        public void AreaR(double heightH, double widthW)
+        {
+            areaR = heightH * widthW;
+            Console.WriteLine(areaR);
+            
+        }
+        public void PerimeterR(double heightH, double widthW)
+        {
+            perimeterR = (heightH + widthW) * 2;
+            Console.WriteLine(perimeterR);
+        }
+    }
+    public class Triangle
+    {
+        public double a;
+        public double b;
+        public double c;
+        public double areaT;
+        public double perimeterT;
+        public void AreaT(double a, double b, double c)
+        {
+            areaT = (a * c) / 2;
+            Console.WriteLine(areaT);
+           
+        }
+        public void PerimeterT(double a, double b, double c)
+        {
+            perimeterT = a + b + c;
+            Console.WriteLine(perimeterT);
+            
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  public AreaC();
+            Circle circle = new Circle();
+            Circle circle1 = new Circle();
+           
+            circle.AreaC(4.0);
+            circle1.PerimeterC(4.0);
+            Triangle triangle = new Triangle();
+            Triangle triangle1 = new Triangle();
+
+            triangle.AreaT(1.0,2.0,3.0);
+            triangle1.PerimeterT(1.0,2.0,3.0);
+            Rectangle rectangle = new Rectangle();
+            Rectangle rectangle1 = new Rectangle();
+
+            rectangle.AreaR(2.0,3.0);
+            rectangle1.PerimeterR(2.0,3.0);
+            Console.ReadLine();
+        }
+    }
+}
